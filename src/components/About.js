b5dbdc62b447198736c7dd3c644ea727cfe5a9c8
Justin <justinknueppel@gmail.com>
2020-07-07 13:47:10 -0400

    Update about page

--- src/components/About.js
+++ src/components/About.js
@@ -7,83 +7,46 @@ export default class About extends React.Component {
   render() {
     return (
       <div className="content">
+        <p>Hi, my name is Justin Knueppel.</p>
         <h3>Education</h3>
         <p>
-          Hi, my name is Justin Knueppel. I am a junior at the Ohio State
-          University studying Data Analytics. I spent the majority of 2019 as a
-          claims analytics intern for Nationwide Insurance where I learned much
-          about the role of Data Analytics in industry.
+          I am a rising senior at the Ohio State University studying Data
+          Analytics. I chose this major because I had always been good with
+          math, but had began programming as I ended high school and wanted that
+          to be a part of my career. Data Analytics was a major that let me
+          pursue both fields. Specifically, I pursued the computational
+          analytics specialization as I have focused more on programming rather
+          than statistics. This has allowed me to take courses on software
+          engineering principals, data structures and algorithms, systems
+          programming, discrete mathematics and more.
         </p>
+        <h3>Work Experience</h3>
+        <p>
+          I spent the majority of 2019 as a claims analytics intern for
+          Nationwide Insurance where I learned much about the role of Data
+          Analytics in industry. For the most part, my focus was on developing
+          automated systems to streamline the analytics process. I was proud to
+          be strong programmer that could help develop systems that made the
+          jobs of everyone around me more straightforward.
+        </p>
+        <h3>Programming</h3>
         <p>
           I began programming in high school by learning Java to create
-          Minecraft mods. Since then, I have taken courses with Ohio State that
-          teach software engineering principals, data structures and algorithms,
-          systems programming, discrete mathematics and more.
-        </p>
-        <h3>Hobbies</h3>
-        <p>
-          All of my life I have been around and involved in music. Singing from
-          middle school to the Men's Glee Club at Ohio State, I have had many
-          valuable opportunities. I was fortunate enough to tour Ireland, work
-          with Dr. Craig Jessop - former director of the Mormon Tabernacle
-          choir, and even sing at Carnegie Hall.
-        </p>
-        <p>
-          Naturally following my love for music, I also have a love for nice
-          headphones. I currently am enjoying my{" "}
-          <a
-            href="https://majorhifi.com/etymotic-hf5-noise-isolating-earphones-review/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Etymotic Research hf5
+          Minecraft mods. Since then, I have studied many facets of computer
+          science and programming both formally though my course work at Ohio
+          State, as well as informally on my own. I have worked through programs
+          such as the{" "}
+          <a href="https://fullstackopen.com/en">Full stack open 2020</a>{" "}
+          through the University of Helsinky, read books such as{" "}
+          <a href="https://www.amazon.com/Clean-Code-Handbook-Software-Craftsmanship/dp/0132350882">
+            Clean Code
           </a>{" "}
-          IEMs and{" "}
-          <a
-            href="https://majorhifi.com/sennheiser-hd-579-review/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Sennheiser HD 579
+          by Robert Martin and{" "}
+          <a href="https://www.amazon.com/Code-Complete-Practical-Handbook-Construction/dp/0735619670">
+            Code Complete
           </a>{" "}
-          open backs. I love listening to music and having high quality
-          headphones truly does make a difference!
-        </p>
-        <p>
-          Additionally, I have been building computers for the past 5 years. My
-          current build includes a{" "}
-          <a
-            href="https://www.amd.com/en/products/cpu/amd-ryzen-5-2600x"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Ryzen 5 2600x
-          </a>
-          ,{" "}
-          <a
-            href="https://www.amd.com/en/products/graphics/radeon-rx-580"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            RX 580
-          </a>
-          , and of course my{" "}
-          <a
-            href="https://mechanicalkeyboards.com/shop/index.php?l=product_detail&p=4164"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vortex Cypher
-          </a>{" "}
-          65% keyboard with Cherry MX Browns and{" "}
-          <a
-            href="https://www.amazon.com/Qisan-Mechanical-Keyboard-Backlight-Magicforce/dp/B01HHTFSIK"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Qisan Magicforce
-          </a>{" "}
-          full sized keyboard with Gatreon reds.
+          by Steve McConnell, and also have completed numerous online tutorials
+          and personal projects.
         </p>
       </div>
     );
